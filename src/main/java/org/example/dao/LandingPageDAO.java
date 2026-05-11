c0307279a75c7c04a952216ebd2926915a5cc940
SamCodex01 <txtstring@gmail.com>
2026-05-10 23:35:37 -0300

    classe DAO, arquivo xml e dependencias mavens adicionados

---
+++ src/main/java/org/example/dao/LandingPageDAO.java
@@ -0,0 +1,11 @@
+package org.example.dao;
+
+public class LandingPageDAO {
+    private String nome;
+    private String email;
+    private String telefone;
+    private String tipoDeServico; //tipo serviço desejado (dropdown: Institucional / E-commerce / Sistema / Não sei)
+    private String mensagem; // (campo texto, 3 linhas) – "Descreva sua ideia ou necessidade"
+}
+
+
