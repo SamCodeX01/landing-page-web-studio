cd35f1ecfa5683c1ea8dc1633a62cba01f4628c3
SamCodex01 <txtstring@gmail.com>
2026-05-12 22:16:53 -0300

    FormularioRepository e main atualizados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.model.Formulario;
+import org.example.repository.FormularioRepository;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+        FormularioRepository repository = new FormularioRepository();
+
+        //Criando  um formulario
+        Formulario formulario = new Formulario();
+        formulario.setNome("Samuel");
+        formulario.setEmail();
+        formulario.setTelefone();
+        formulario.setTipoDeServico();
+        formulario.setMensagem();
+
+        //Salvando um formulario
+        repository.salvar(formulario);
+        System.out.println("Formulario salvo com ID: " + formulario.getId());
+
+        //Buscar formulario
+
     }
 }
--- src/main/java/org/example/repository/FormularioRepository.java
+++ src/main/java/org/example/repository/FormularioRepository.java
@@ -1,4 +1,24 @@
 package org.example.repository;
 
+import jakarta.persistence.EntityManager;
+import org.example.model.Formulario;
+import org.example.util.JPAUtil;
+
 public class FormularioRepository {
+
+    public void salvar(Formulario formulario){
+        EntityManager em = JPAUtil.getEntityManager();
+
+        try{
+            em.getTransaction().begin();
+            em.persist(formulario);
+            em.getTransaction().commit();
+        }catch (Exception e){
+            em.getTransaction().rollback();
+            throw e;
+        }finally {
+            em.close();
+        }
+
+    }
 }
